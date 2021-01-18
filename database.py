21148661a23839bfd81126a3107bc7f6ad441cc5
naterenegar <naterenegar@gmail.com>
2021-01-18 18:08:17 -0500

    Writing to JSON file now works

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-
 # We also want this to be able to scan an experiment directory containing
 # multiple experiments (as directories). In each directory is some data, and
 # the user can pass a parsing function in to construct the database.
@@ -7,16 +6,40 @@
 # map of where to find the data. This makes sense since the usual use case is a
 # small number of files at a time.
 
-# We also want to store the cell type 
+# TODO: We also want to store the cell type 
+
+import re
+import os
+import json
 
 class Database(object):
 
-    def __init__(self, init_file=None):
-        self.exps = []
-        if init_file:
-            # TODO: read JSON and restore state
-            pass 
+    # autosave(bool): Determines if file is automatically saved on program exit
+    def __init__(self, init_filename='.db.json', autosave=True):
+        self.db_dict = {'info': {'initialized': False}, 'data': {}}
+        self.dbf_init = open(init_filename, "r+")
+        self.dbf_working = open(init_filename + '.tmp', "w+")
+        self.filename = init_filename
+        self.autosave = autosave 
+        self.dirty = True
 
+        try:
+            data = json.load(self.dbf_init)
+        except json.decoder.JSONDecodeError: 
+            print(init_filename, "either does not exist or is empty...\n"\
+                    "Starting new database")
+
+    # This calls writes back any changes to the local JSON file 
+    def __del__(self):
+        if self.autosave == True and self.dirty == True:
+            self.save()
+
+    def save(self):
+        self.dbf_init.close()
+        json.dump(self.db_dict, self.dbf_working, indent=4)     
+        self.dbf_working.close()
+        os.system('mv ' + self.filename + '.tmp' + ' ' + self.filename)
+        self.dirty = False
 
     def __str__(self):
         retstr = "" 
@@ -24,15 +47,55 @@ class Database(object):
             retstr = retstr + str(exp) + "\n"
 
         return retstr
-   
+  
+    def is_initialized(self):
+        return self.db_dict['info']['initialized']
+
     # TODO: Currently does not support importing annotations
-    def init_database(self, top_dir, exp_prefix):
-        pass
+    def init_database(self, top_dir, exp_dir, exp_prefix):
+        if self.db_dict['info']['initialized'] == True:
+            print("Warning: Overwriting initialized database")
 
+        exps = list(filter(lambda x: x[0:len(exp_prefix)] == exp_prefix,
+            os.listdir(os.path.join(top_dir, exp_dir))))
+        print("Found " + str(len(exps)) + " experiments.")
 
-    def add_experiment(self, experiment_name, experiment_data):
-        # format of experiment_data left up to use case. For ours we'll have
-        # two types of data associated with an experiment: images and
-        # capacitance measurements. Each datum should be timestamped w.r.t the
-        # same time origin
-        self.exps.append((experiment_name, experiment_data))
+        exps = {x: {"full_path": os.path.join(top_dir, exp_dir, x)} for x in exps}
+
+        for exp in exps:
+            exps[exp]['datatypes'] = os.listdir(exps[exp]['full_path'])
+            for datatype in exps[exp]['datatypes']:
+                exps[exp][datatype] = {}
+
+        total_images = 0
+        annotated_images = 0
+        for exp, data in exps.items():
+            if 'images' in data['datatypes']:
+                image_dir = os.path.join(data['full_path'], 'images')
+                image_list = os.listdir(image_dir) 
+                num_images = len(image_list)
+                total_images = total_images + num_images
+                exps[exp]['images']['num_images'] = num_images
+
+                img_array = []
+                for img in image_list:
+                    name = img
+                    full_img_path = os.path.join(image_dir, name)
+                    source_exp = exp 
+                    cell_type = None # TODO: Get the cell type from the .mat file
+                    time = name.split(exp + '_t')[1].split('.')[0]
+                    nums = re.search('([0-9]+)_([0-9]+).*', time) 
+                    time = float(nums.group(1)) + (float(nums.group(2)) / 1000)
+                    annotated = False
+                    annotation_path = ""
+                    image_dict = {'name': name, 'source_exp': exp, 'path': full_img_path,
+                            'cell_type': cell_type, 'time': time, 'annotated': annotated,
+                            'annotation_path': annotation_path}
+                    img_array.append(image_dict)
+                img_array.sort(key=lambda x: x['time'])       
+               
+                exps[exp]['images']['image_array'] = img_array
+
+        print("Found a total of", total_images, "images across all experiments, {:.2f}% of which are annotated.".format(annotated_images/total_images))
+        self.db_dict['info']['initialized'] = True
+        self.db_dict['data']['experiments'] = exps
--- init_database.py
+++ init_database.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import database
-import os
-
-dbf = open(".db.json", "w+")
 
 db = database.Database()
-db.add_experiment('exp01', None)
+
+# Read JSON file for database object
+# Check if the database has been initialized
+#  
 
 # What we want to do now is we want to look at a root directory and initalize
 # everything into the database. In other words, we want to define a file
@@ -23,9 +23,8 @@ db.add_experiment('exp01', None)
 # training, validation, and other analyses.
 
 # As far as storing state, I think we'll track each files location, type,
-# timestamp, etc. in a top level JSON. This should make looking up data easy
-
-# So let's get started!
+# timestamp, etc. in a top level JSON. This should make looking up data easy,
+# i.e. deserialize JSON into datastructure -> query datastructure
 
 no_init_file = True
 
@@ -33,9 +32,17 @@ if no_init_file:
     top_dir = input("Please enter the top level directory of the database: ")
     exp_dir = input("Please enter the experiment subdir: ")
     exp_prefix = input("Please enter the experiment prefix: ")
-    
-    exps = list(filter(lambda x: x[0:len(exp_prefix)] == exp_prefix,
-        os.listdir(os.path.join(top_dir, exp_dir))))
-    print("Found " + str(len(exps)) + " experiments:")
-    print(exps)
 
+    db.init_database(top_dir, exp_dir, exp_prefix)
+    quit()
+
+    # Let's create a JSON object for an experiment  
+    # Python can automagically serialize the following types into JSON:
+    #   dict -> object
+    #   list,tuple -> array
+    #   str -> string
+    #   int,long,float -> number 
+    #   True -> true
+    #   False -> false
+    #   None -> null
+    # These should be sufficient
