19c082e0e5dc280b234c5fa8beb8f8cc798540fb
naterenegar <naterenegar@gmail.com>
2021-02-01 17:42:34 -0500

    Added ROI generation for annotation creation

--- database.py
+++ database.py
@@ -1,40 +1,59 @@
-# We also want this to be able to scan an experiment directory containing
-# multiple experiments (as directories). In each directory is some data, and
-# the user can pass a parsing function in to construct the database.
-
-# Maybe instead of reading all of the data into memory, we can just construct a
-# map of where to find the data. This makes sense since the usual use case is a
-# small number of files at a time.
-
-# TODO: We also want to store the cell type 
-
 import re
 import os
 import json
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+# Nice to haves to get to if you have time:
+#   - Pack/Unpack NPZs 
+
+# TODO: Add support for importing annotations
+
+# TODO: Add warning for annotations that cannot be sourced
+
+# NOTE: Assumes images are preprocessed (e.g. aligned, cropped, normalized,
+# etc.). If we want, we can add a description for an experiment that
+# denotes what steps have been taken. This is fine for now. Alternatively,
+# to maintain consistency throughout the database, we could, upon init,
+# take all images in a experiment through preprocessing steps
+
+# TODO: I want to be able to open up all of the raw images corresponding
+# to images in an NPZ in a matplotlib window. This is useful because you
+# can go back and forth, look at other contexts, etc
 
 class Database(object):
 
-    # autosave(bool): Determines if file is automatically saved on program exit
     def __init__(self, init_filename='.db.json', autosave=True):
-        self.db_dict = {'info': {'initialized': False}, 'data': {}}
-        self.dbf_init = open(init_filename, "r+")
+        self.db_dict = {'info': {'initialized': False}, 'data': {}} # default dictionary
+        perm_string = "w+"
+        if os.path.exists(init_filename):
+            perm_string = "r+"
+        self.dbf_init = open(init_filename, perm_string)
         self.dbf_working = open(init_filename + '.tmp', "w+")
         self.filename = init_filename
         self.autosave = autosave 
         self.dirty = True
+        self.prompt = "ann-database> "
+
+        self.cmd_handlers = {'import-anns': self.cmd_handler_import_annotation,
+                             'exit': self.save,
+                             'create-anns': self.cmd_handler_create_anns}
 
         try:
-            data = json.load(self.dbf_init)
+            self.db_dict = json.load(self.dbf_init)
         except json.decoder.JSONDecodeError: 
-            print(init_filename, "either does not exist or is empty...\n"\
+            print("Database info file", init_filename, "either does not exist or is empty...\n"\
                     "Starting new database")
 
-    # This calls writes back any changes to the local JSON file 
+    # this "destructor" writes back any changes to the local JSON file upon 
     def __del__(self):
         if self.autosave == True and self.dirty == True:
             self.save()
 
-    def save(self):
+    def save(self, dummy=None):
         self.dbf_init.close()
         json.dump(self.db_dict, self.dbf_working, indent=4)     
         self.dbf_working.close()
@@ -51,14 +70,30 @@ class Database(object):
     def is_initialized(self):
         return self.db_dict['info']['initialized']
 
-    # TODO: Currently does not support importing annotations
-    def init_database(self, top_dir, exp_dir, exp_prefix):
+   
+    # NOTE: This currently assumes all images in a experiment are the same
+    # resolution. We can add an option to check for this if necessary, but I
+    # think it is a reasonable assumption for now
+    def init_database(self, top_dir='db', exp_dir='exps', ann_dir='anns', exp_prefix='exp'):
+        if not (os.path.exists(top_dir) and os.path.exists(os.path.join(top_dir, exp_dir))):
+            print("ERROR: Provided invalid paths:", top_dir, ",", os.path.join(top_dir, exp_dir))
+            print("WARNING: Database not initialized.")
+            return
+
         if self.db_dict['info']['initialized'] == True:
             print("Warning: Overwriting initialized database")
 
+        # TODO: If these already exist in database file, check to see if we can
+        # find these structures. If not, ask user to update directory structure
+        self.db_dict['info']['top_dir'] = top_dir 
+        self.db_dict['info']['exp_dir'] = os.path.join(top_dir, exp_dir)
+        self.db_dict['info']['ann_dir'] = os.path.join(top_dir, ann_dir)
+        self.db_dict['info']['exp_prefix'] = exp_prefix 
+
         exps = list(filter(lambda x: x[0:len(exp_prefix)] == exp_prefix,
             os.listdir(os.path.join(top_dir, exp_dir))))
-        print("Found " + str(len(exps)) + " experiments.")
+        exps.sort()
+        print("Found " + str(len(exps)) + " experiments:", exps)
 
         exps = {x: {"full_path": os.path.join(top_dir, exp_dir, x)} for x in exps}
 
@@ -69,6 +104,9 @@ class Database(object):
 
         total_images = 0
         annotated_images = 0
+
+        # Looks through experiment directories for raw images. Takes note of
+        # any annotation pointers
         for exp, data in exps.items():
             if 'images' in data['datatypes']:
                 image_dir = os.path.join(data['full_path'], 'images')
@@ -76,6 +114,12 @@ class Database(object):
                 num_images = len(image_list)
                 total_images = total_images + num_images
                 exps[exp]['images']['num_images'] = num_images
+
+                # NOTE: This assumes all images in an experiment are the same
+                # resolution. If this is not true, move these lines inside the
+                # image loop
+                res = cv2.imread(os.path.join(image_dir, image_list[0]))
+                h, w, _ = res.shape
 
                 img_array = []
                 for img in image_list:
@@ -86,16 +130,184 @@ class Database(object):
                     time = name.split(exp + '_t')[1].split('.')[0]
                     nums = re.search('([0-9]+)_([0-9]+).*', time) 
                     time = float(nums.group(1)) + (float(nums.group(2)) / 1000)
-                    annotated = False
-                    annotation_path = ""
-                    image_dict = {'name': name, 'source_exp': exp, 'path': full_img_path,
-                            'cell_type': cell_type, 'time': time, 'annotated': annotated,
-                            'annotation_path': annotation_path}
+                    annotations = [] # tuples of (path, (x1, y1, x2, y2)) to find annotation and rectangle within self
+                    image_dict = {'name': name, 'resolution': [w, h], 'source_exp': exp, 'path': full_img_path,
+                            'cell_type': cell_type, 'time': time, 'annotations': annotations }
                     img_array.append(image_dict)
                 img_array.sort(key=lambda x: x['time'])       
                
                 exps[exp]['images']['image_array'] = img_array
 
+        ann_dir = os.path.join(top_dir, ann_dir)
+        ann_files = os.listdir(ann_dir)
+        json_files = []
+        for af in ann_files:
+            if '.json' in af:
+                json_files.append(af)
+            elif '.npz' not in af:
+                ann_files.remove(af)
+
+        # TODO: Handle return value (i.e. concatenate annotation dictionaries)
+        for npz in ann_files:
+            self.cmd_handler_import_annotation(os.path.join(ann_dir, npz), list_call=True)
+
+
+        if annotated_images != 0 and not json_files_found:
+            print("WARNING: Experiments claim images are annotated but we did \
+                    not find any annotation metadata")
+
         print("Found a total of", total_images, "images across all experiments, {:.2f}% of which are annotated.".format(annotated_images/total_images))
         self.db_dict['info']['initialized'] = True
         self.db_dict['data']['experiments'] = exps
+
+    # TODO: 
+    # Give number of desired images as well as images per time sequence.
+    # This function will search through the database and look for images that
+    # fit the criteria. you can also choose a granularity for how often to
+    # create an NPZ. This function also creates all of the necessary metadata
+    # for annotation tracking.
+
+    # Today:
+    #   - Given number of annotations, creates time series and separate NPZs
+    #   while checking to make sure none of them overlap. We'll confirm with
+    #   the user the location of each time series before creating the NPZs 
+    #       1. get desired # of annotations, time series, annotation resolution, npz granularity from user
+    #           - # of annotations: total number of annotations to create
+    #           - # of time series: number of consecutive sequences to create
+    #           (e.g. 2 time series w/ 100 annotations creates 2 sets of 50
+    #           looking at two different spots on the image)
+    #           - annotation resolution (default 256x256): image size of each annotation
+    #           - npz granularity: number of annotations to pack into each NPZ file. 
+    def cmd_handler_create_anns(self, args):
+        # commmand is "ann-create"
+        exp = int(input("Please enter desired experiment number: "))
+        num_anns = int(input("Please enter number of annotations: "))
+        npz_gran = int(input("Please enter number of images per NPZ: ")) 
+
+        # grab all image metadata in relevant experiment
+        exp = 'exp' + ('0' + str(exp) if exp < 10 else str(exp))
+        exp_images = self.db_dict['data']['experiments'][exp]['images']['image_array']
+        w, h = exp_images[0]['resolution']
+
+        # This loop lets user configure ROI options
+        acceptable = 'n'
+        while acceptable == 'n':
+            ann_res = input("Please enter annotation resolution in form \"<width>, <height>\": ")
+            ann_w, ann_h = ann_res.split(',')
+            ann_w = int(ann_w)
+            ann_h = int(ann_h)
+            w_strech = float(input("Please enter horizontal spacing factor (min 1): "))
+            h_strech = float(input("Please enter vertical spacing factor (min 1): "))
+            w_strech = 1 if w_strech < 1 else w_strech
+            h_strech = 1 if h_strech < 1 else h_strech
+
+            # generate equally spaced tuples by default
+            max_ws = (w - 2 * ann_w) // int(ann_w * w_strech)
+            max_hs = (h - 2 * ann_h) // int(ann_h * h_strech)
+            w_starts = np.linspace(ann_w, w - 2*ann_w, max_ws)
+            h_starts = np.linspace(ann_h, h - 2*ann_h, max_hs) 
+
+            maximum_ts = max_ws * max_hs
+            time_series = int(input("Please enter number of time series (maximum = " + str(maximum_ts) + "): "))
+            time_stride = int(input("Please enter time series stride (number of images between each step in series): "))
+
+            starts = []
+            for i in range(time_series):
+                starts.append((int(w_starts[i % max_ws]), int(h_starts[i // max_ws])))
+
+            fig,ax = plt.subplots(1)
+            im = cv2.imread(exp_images[0]['path']) 
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+            ax.imshow(im)
+            i = 1
+            for start in starts:
+                rect = patches.Rectangle(start,ann_w,ann_h,linewidth=1,edgecolor='g',facecolor='none')
+                ax.add_patch(rect)
+                ax.annotate(str(i), xy=(start[0] + int(ann_w / 2), start[1] + int(ann_h / 2)), 
+                            color='b', fontsize='x-large', ha='center', va='center') 
+                i = i + 1
+
+            print("Showing generated ROIs...")
+            plt.show()
+
+            acceptable = input("Were those ROIs acceptable? [y/n]: ")
+
+
+
+    # How this is going to work:
+    #  - One JSON file in the anns directory that contains information about
+    #  all annotations in the database. This order in this file does not
+    #  correspond to order in an NPZ. Each annotation will be an entry
+    #  containing:
+    #   - NPZ file
+    #      - annotation status 
+    #      There will be an array of entries in the NPZ
+    #      - annID (integer)
+    #      - size (resolution)
+    #      - time (hours)
+    #      - experiment (integer)
+    #      - cellType (string)
+    #      - numberOfCells (of each class if more than one class)
+    #      - sourceImage
+    #      - sourceOffset (integer x, integer y), location of top left pixel of annotation in source
+    def cmd_handler_import_annotation(self, args, list_call=False):
+        ann_file = args
+        if not list_call:
+            ann_file = input("Please enter path to NPZ file containing annotations: ")        
+            if not os.path.isfile(ann_file):
+                print("Invalid file or path. Please try again")
+                return
+
+        ann = np.load(ann_file)
+        print("Found following files in NPZ: ", str(ann.files))
+        x_exists = 'X' in ann.files
+        y_exists = 'y' in ann.files
+        md_exists = 'md' in ann.files
+        if not x_exists:
+            choice = input("WARNING: Could not find source images in NPZ under name \'X\'. Is there an alternate name? [y/n]: ")
+
+        if not y_exists:
+            choice = input("WARNING: Could not find annotation images in NPZ under name \'y\'. Is there an alternate name? [y/n]: ")
+
+        stray_anns = False
+        if not md_exists:
+            choice = input("WARNING: Could not find annotation metadata in NPZ under name \'md\'. Is there an alternate name? [y/n]: ")
+            if choice != 'y':
+                filename = input("Is there an alternate file? (enter for NO, filename for YES): ")
+                if filename != '':
+                    # open up file
+                    pass
+                else:
+                    stray_anns = True
+            else:
+                stray_anns = True
+
+        # Stray annotations should get set if we cannot complete the source
+        # image to annotation mapping for whatever reason. This could be lack
+        # of metadata or lack of source images in the database
+        if stray_anns:
+            print("WARNING: Proceeding with stray annotations. Unique datasets cannot be gauranteed")
+
+        return
+
+    # WARNING: This hands over control from the main program to the object 
+    def start_command_CLI(self):
+        cmd = ""
+
+        while cmd != "exit":
+            cmd = input(self.prompt) 
+            cmd = cmd.split(' ')
+            cmd, args = cmd[0], cmd[1:]
+            try:
+                self.cmd_handlers[cmd](args)
+            except KeyError:
+                if cmd.lower() != 'help':
+                    print("Command not found.")
+                print("Here's a list of available commands: ")
+                cmds = list(self.cmd_handlers.keys())
+                cmds.sort()
+                for cmd in cmds: 
+                    print("    " + cmd)
+
+    
+
--- init_database.py
+++ init_database.py
@@ -4,45 +4,15 @@ import database
 
 db = database.Database()
 
-# Read JSON file for database object
-# Check if the database has been initialized
-#  
+if not db.is_initialized():
+    top_dir = input("Please enter the top level directory of the database, or press enter for default (db): ")
+    exp_dir = input("Please enter the experiment subdir, or press enter for default (exps):")
+    exp_prefix = input("Please enter the experiment prefix, or press enter for default (exp): ")
+    ann_dir = input("Please enter the annotation subdir, or press enter for default (anns): ")
 
-# What we want to do now is we want to look at a root directory and initalize
-# everything into the database. In other words, we want to define a file
-# structure the database can adhere to.
-
-# I think we can have all of the raw data organized by experiment, and in each
-# experiment directory we have data organized by type into subdirectories. So
-# for example, we'd have a top level /database/ with all of the experiments in
-# it, and in each experimennt, e.g. exp01/, we'd have a subfolder for each type
-# of raw data. In our case, this is image data (.jpgs) and electrode data.
-# In each experiment folder, we can also have a <datatype>_ann directory for
-# annotated versions of the data. We'll organize this on an experiment level,
-# but also have a top level annotation folder for construction of datasets for
-# training, validation, and other analyses.
-
-# As far as storing state, I think we'll track each files location, type,
-# timestamp, etc. in a top level JSON. This should make looking up data easy,
-# i.e. deserialize JSON into datastructure -> query datastructure
-
-no_init_file = True
-
-if no_init_file:
-    top_dir = input("Please enter the top level directory of the database: ")
-    exp_dir = input("Please enter the experiment subdir: ")
-    exp_prefix = input("Please enter the experiment prefix: ")
-
-    db.init_database(top_dir, exp_dir, exp_prefix)
-    quit()
-
-    # Let's create a JSON object for an experiment  
-    # Python can automagically serialize the following types into JSON:
-    #   dict -> object
-    #   list,tuple -> array
-    #   str -> string
-    #   int,long,float -> number 
-    #   True -> true
-    #   False -> false
-    #   None -> null
-    # These should be sufficient
+    db.init_database(top_dir=(top_dir if top_dir != '' else 'db'), 
+                     exp_dir=(exp_dir if exp_dir != '' else 'exps'),
+                     exp_prefix=(exp_prefix if exp_prefix != '' else 'exp'),
+                     ann_dir=(ann_dir if ann_dir != '' else 'anns')) 
+db.start_command_CLI()
+quit()
