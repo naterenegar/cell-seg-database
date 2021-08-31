6f398d80f048596b0fd6fb39f90b79e8f0b8bb94
naterenegar <naterenegar@gmail.com>
2021-08-30 21:18:16 -0400

    update of annotation metadata upon completion

--- database_remote_commands.py
+++ database_remote_commands.py
@@ -82,46 +82,96 @@ class Database(object):
 
         print("Found " + str(len(anns)) + " annotations matching the given criteria.")
 
+    
+    def fetch_and_pack_image(self, source_s3_key, ann_s3_key, crop, size, annotation_blank=True):
+        # Fetch the source image
+        tmp_filename = '.tmp.src.png'
+        ann_filename = '.tmp.ann.png'
+
+        with open(tmp_filename, 'wb') as f:
+            self.s3_handle.download_fileobj(self.bucket, source_s3_key, f)
+
+        im = load_image(tmp_filename)
+        X = np.expand_dims(crop_image(im, crop), axis=0)
+        y = np.zeros(size)
+
+        if not annotation_blank:
+            with open(ann_filename, 'wb') as f:
+                self.s3_handle.download_fileobj(self.bucket, ann_s3_key, f)
+                y = load_image(ann_filename)
+                print(y.shape) 
+
+        y = np.expand_dims(np.expand_dims(y, axis=-1), axis=0)
+        np.savez('.tmp.npz', X=X, y=y)
+
     # Args is a list of tags
     async def cmd_handler_do_annotation(self, **args):
+        # Clean up any existing temporary annotations
+        if os.path.exists('.tmp.npz'):
+            os.remove('.tmp.npz')
+
+        if os.path.exists('.tmp_save_version_0.npz'):
+            os.remove('.tmp_save_version_0.npz')
+
         # First, we make sure we got an id
         if "id" not in args.keys():
             print("    Must provide named argument \"id\" with do_annotation")
         
         ann = await ImageAnnotation.objects.get(id=int(args["id"]))
+        source_image = await SourceImage.objects.get(name=ann.source_image.name)
+        size = ann.get_image_size() 
+        crop = ann.get_source_offset()
         key = ""
-        if ann.s3_key == "":
+        if ann.s3_key == "" or ann.finished == False:
             key = "db/anns/" + str(args["id"]) + "/y_" + str(args["id"]) + ".png"
             print("    Annotation is blank. Creating new annotation")
 
-            source_image = await SourceImage.objects.get(name=ann.source_image.name)
-            size = ann.get_image_size() 
-            crop = ann.get_source_offset()
 
-            tmp_filename = '.tmp.anns/src.png'
             print("    Fetching source image...")
-            with open(tmp_filename, 'wb') as f:
-                self.s3_handle.download_fileobj(self.bucket, source_image.s3_key, f)
+            self.fetch_and_pack_image(source_image.s3_key, 
+                                      "", 
+                                      crop, 
+                                      size, 
+                                      annotation_blank=True)
             print("    Source image downloaded. Opening annotation tool")
-            im = load_image(tmp_filename)
-            X = np.expand_dims(crop_image(im, crop), axis=0)
-            y = np.expand_dims(np.expand_dims(np.zeros(size), axis=-1), axis=0)
-            print(y.shape)
-            np.savez('.tmp.npz', X=X, y=y)
+
+            ann.created_by = ""
+            ann.created_on = datetime.datetime.now()
+        elif get_yes_no("Annotation exists in database. Update?"):
+            key = ann.s3_key
+
+            print("    Fetching source image and annotation...")
+            self.fetch_and_pack_image(source_image.s3_key, 
+                                      key, 
+                                      crop, 
+                                      size, 
+                                      annotation_blank=False)
+            print("    Source image and annotation downloaded. Opening annotation tool")
 
         else:
-            key = ann.s3_key
-            print("    Annotation exists in database. Update? [y/n]: ")
+            return
 
-            with open('.tmp.anns/ann.png', 'wb') as f:
-                self.s3_handle.download_fileobj(self.bucket, ann.s3_key, f)
-    
+        ann.started_at = datetime.datetime.now()
         caliban_proc = Popen(['python3', 'deepcell-label/desktop/caliban.py', '-rgb', 'RGB', '.tmp.npz'])
 
         if get_yes_no("Did you complete the annotation?") and os.path.isfile('.tmp_save_version_0.npz'):
+
+            # Upload annotation
             finished_ann = np.load('.tmp_save_version_0.npz')
-            save_image(np.squeeze(finished_ann['y']), ".tmp.upload.png", grayscale=True)
+
+            y = finished_ann['y']
+            y = (((y - y.min()) / (y.max() - y.min())) * 255.9).astype(np.uint8) 
+            save_image(np.squeeze(y).astype(np.uint8), ".tmp.upload.png", grayscale=True)
             self.s3_handle.upload_file(".tmp.upload.png", self.bucket, key)
+
+            # Update annotation metadata
+            ann.s3_key = key
+            ann.finished = True
+            ann.in_progress = False
+            ann.updated_on = datetime.datetime.now()
+            ann.updated_by = ""
+            ann.finished_at = datetime.datetime.now()
+            await ann.update()
 
 
     # WARNING: This hands over control from the main program to the object 
